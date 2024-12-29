27be132353fe72cbee0d888c707a84e36277f317
Kenta Ikeda <77259023+Krudaovo@users.noreply.github.com>
2024-12-29 10:47:02 +0900

    first commit

---
+++ target/debug/deps/types-0b361a5c19fcddb6.d
@@ -0,0 +1,5 @@
+C:\Users\airon\Desktop\rust\types\target\debug\deps\libtypes-0b361a5c19fcddb6.rmeta: src/main.rs
+
+C:\Users\airon\Desktop\rust\types\target\debug\deps\types-0b361a5c19fcddb6.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/types-67c70b13bc6f5c67.d
@@ -0,0 +1,5 @@
+C:\Users\airon\Desktop\rust\types\target\debug\deps\libtypes-67c70b13bc6f5c67.rmeta: src/main.rs
+
+C:\Users\airon\Desktop\rust\types\target\debug\deps\types-67c70b13bc6f5c67.d: src/main.rs
+
+src/main.rs:
