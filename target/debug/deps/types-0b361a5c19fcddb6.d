65ce07ce24181bf74cdc8dc57b59bbb94470aac2
Kenta Ikeda <77259023+Krudaovo@users.noreply.github.com>
2024-12-29 10:49:33 +0900

    remove cache

--- target/debug/deps/types-0b361a5c19fcddb6.d
+++
@@ -1,5 +0,0 @@
-C:\Users\airon\Desktop\rust\types\target\debug\deps\libtypes-0b361a5c19fcddb6.rmeta: src/main.rs
-
-C:\Users\airon\Desktop\rust\types\target\debug\deps\types-0b361a5c19fcddb6.d: src/main.rs
-
-src/main.rs:
